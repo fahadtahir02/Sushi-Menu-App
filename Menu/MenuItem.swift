f900721017b70385521946b893d38a3053ba1b5c
fahadtahir02 <fahad.tahir79@myhunter.cuny.edu>
2024-01-15 23:25:35 -0500

    Created MenuItem struct + array with all our menu items

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Fahad Tahir on 1/15/24.
+//
+
+import Foundation
+//Using this struct to model menu items in our app sort of like a component in react
+struct MenuItem {
+    var name:String
+    var price:String
+    var imageName:String
+}
